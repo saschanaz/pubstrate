69ee3fcc8863e5d70d7abeb3d173318f5681d443
Christopher Allan Webber <cwebber@dustycloud.org>
2015-05-26 06:14:15 -0500

    copyright header

--- object_heirarchy.scm
+++ object_heirarchy.scm
@@ -1,5 +1,22 @@
-;; Just an experiment to define the full set of relationships in AS2.0
-;; as classes.. not actually intended to be used
+;; Copyright (C) 2015 Christopher Allan Webber <cwebber@dustycloud.org>
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301 USA
+
+;;; Just an experiment to define the full set of relationships in AS2.0
+;;; as classes.. not actually intended to be used
 
 (use-modules (oop goops))
 
