9fe0bca323c6fefc60cc76903042ab28b5491af0
Christopher Allan Webber <cwebber@dustycloud.org>
2015-05-26 11:54:59 -0500

    Working on impleemetning the activitystreams vocab

--- object_heirarchy.scm
+++ object_heirarchy.scm
@@ -21,62 +21,112 @@
 (use-modules (oop goops))
 
 (define-class <json-ldable> ()
-  @context @type @id)
+  (fields #:allocation #:each-subclass)
+  ;; (mandatory #:allocation #:each-subclass)
+  (uri #:allocation #:each-subclass)
+  )
+(class-slot-set! <json-ldable> 'fields
+                 '(@context @type @id))
+
+;; emacs: (put 'define-asclass 'scheme-indent-function 2)
+
+;;; This is a shortcut for defining activitystreams-style objects
+;;; since there is often a lot of boilerplate
+
+(define-syntax-rule (define-asclass class (parent ...)
+                      as-uri as-fields . rest)
+  (begin
+    (define-class class (parent ...)
+      . rest)
+    (class-slot-set! class 'uri as-uri)
+    (class-slot-set! class 'fields as-fields)))
+
+(define-generic render-as-json)
+
+;; Utility to gather the list of all 
+
+(define* (gather-fields class #:optional (prop 'fields))
+  "Gather available fields recursively from AS definitions
+
+Use like:
+  (gather-fields <Activity>)"
+  (let ((listy-results
+         (catch 'goops-error
+           (lambda () (class-slot-ref class prop))
+           (lambda (key . args) #:unbound))))
+    (if (eq? #:unbound listy-results)
+        '()
+        (apply append
+               listy-results
+               (map (lambda (class) (gather-fields class prop))
+                    (class-direct-supers class))))))
+
 
 ;; ========================================
 ;; Core classes
 ;; ========================================
 
-(define-class <ASObject> (<json-ldable>)
-  ;; http://www.w3.org/ns/activitystreams#Object
-  alias attachment attributedTo content
-  context displayName endTime generator icon
-  image inReplyTo location preview published
-  replies scope startTime summary tag title
-  updated url)
+(define-asclass <ASObject> (<json-ldable>)
+  "http://www.w3.org/ns/activitystreams#Object"
+  '(alias attachment attributedTo content
+          context displayName endTime generator icon
+          image inReplyTo location preview published
+          replies scope startTime summary tag title
+          updated url))
 
-(define-class <ASLink> (<json-ldable>)
-  ;; http://www.w3.org/ns/activitystreams#Link
-  href rel mediaType displayName title
-  hreflang height width duration)
+(define-asclass <ASLink> (<json-ldable>)
+  "http://www.w3.org/ns/activitystreams#Link"
+  '(href rel mediaType displayName title
+         hreflang height width duration))
 
-(define-class <Activity> (<ASObject>)
-  ;; http://www.w3.org/ns/activitystreams#Activity
-  actor object target result origin priority
-  to bto cc bcc)
 
-;; In this one, the actor is a direct object of action as opposed to
-;; the object property
-(define-class <IntransitiveActivity> (<Activity>)
-  ;; http://www.w3.org/ns/activitystreams#IntransitiveActivity
-  )
+(define-asclass <ASLink> (<json-ldable>)
+  "http://www.w3.org/ns/activitystreams#Link"
+  '(href rel mediaType displayName title
+         hreflang height width duration))
 
-(define-class <Actor> (<ASObject>)
-  ;; http://www.w3.org/ns/activitystreams#Actor
-  )
+(define-asclass <Activity> (<ASObject>)
+  "http://www.w3.org/ns/activitystreams#Activity"
+  '(actor object target result origin priority
+          to bto cc bcc))
 
-(define-class <Collection> (<ASObject>)
-  ;; http://www.w3.org/ns/activitystreams#Collection
-  totalItems itemsPerPage current next prev
-  first last self items)
+;;; In this one, the actor is a direct object of action as opposed to
+;;; the object property
+;;; 
+;;; ... technically this does NOT inherit the "object" field, but our
+;;; algorithm for determining available properties assumes it does...
 
-(define-class <OrderedCollection> (<Collection>)
-  ;; http://www.w3.org/ns/activitystreams#OrderedCollection
-  startIndex)
+(define-asclass <IntransitiveActivity> (<Activity>)
+  "http://www.w3.org/ns/activitystreams#IntransitiveActivity"
+  '())
+
+(define-asclass <Actor> (<ASObject>)
+  "http://www.w3.org/ns/activitystreams#Actor"
+  '())
+
+(define-asclass <Collection> (<ASObject>)
+  "http://www.w3.org/ns/activitystreams#Collection"
+  '(totalItems itemsPerPage current next prev
+               first last self items))
+
+(define-asclass <OrderedCollection> (<Collection>)
+  "http://www.w3.org/ns/activitystreams#OrderedCollection"
+  '(startIndex))
 
 
 ;; ========================================
 ;; Extended classes: Activity types
 ;; ========================================
 
-(define-class <Accept> (<Respond>)
-  ;; http://www.w3.org/ns/activitystreams#Accept
-  )
+(define-asclass <Accept> (<Respond>)
+  "http://www.w3.org/ns/activitystreams#Accept"
+  '())
 
-(define-class <TentativeAccept> (<Accept>)
-  ;; http://www.w3.org/ns/activitystreams#TentativeAccept
-  )
+(define-asclass <TentativeAccept> (<Accept>)
+  "http://www.w3.org/ns/activitystreams#TentativeAccept"
+  '())
 
-(define-class <Add> (<Activity>)
-  ;; http://www.w3.org/ns/activitystreams#Add
-  )
+(define-asclass <Add> (<Activity>)
+  "http://www.w3.org/ns/activitystreams#Add"
+  '())
+
