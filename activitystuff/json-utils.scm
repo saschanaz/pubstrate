295c527624d2852384162784c144b535ef1f7233
Christopher Allan Webber <cwebber@dustycloud.org>
2015-09-02 15:09:57 -0500

    Okay, made some converters from sjson to vjson

--- activitystuff/json-utils.scm
+++ activitystuff/json-utils.scm
@@ -18,9 +18,14 @@
 
 (define-module (activitystuff json-utils)
   #:use-module (activitystuff contrib json)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 vlist)
   #:export (json-alist?
             json-assoc json-ref json-acons
-            read-json-from-string write-json-to-string))
+            read-json-from-string write-json-to-string
+            vhash-ref
+            sjson->vjson vjson->sjson))
 
 ;; JSON helper procedures
 
@@ -38,7 +43,6 @@
 (define (json-acons key value json-alist)
   (cons '@ (acons key value (cdr json-alist))))
 
-
 (define (read-json-from-string string)
   (call-with-input-string string
     (lambda (p)
@@ -48,3 +52,66 @@
   (call-with-output-string
    (lambda (p)
      (write-json exp p))))
+
+
+;; This just makes my life slightly easier
+(define (vhash-ref vhash key)
+  "Like assoc-ref but for a vhash"
+  (cdr (vhash-assoc key vhash)))
+
+
+(define (sjson->vjson sjson)
+  "Convert sjson format to vjson
+Really this means converting @-prefixed alists to vhashes"
+  (define (convert-list exp)
+    (map convert-json exp))
+
+  (define (convert-json-alist exp)
+    (fold
+     (lambda (item prev)
+       (match item
+         ((key . val)
+          (vhash-cons key (convert-json val) prev))))
+     vlist-null
+     ;; cdr because first item is the @
+     (cdr exp)))
+
+  (define (convert-json exp)
+    (match exp
+      ((? json-alist? exp)
+       (convert-json-alist exp))
+      ((? pair? exp)
+       (convert-list exp))
+      (anything-else anything-else)))
+
+  (convert-json sjson))
+
+
+(define (vjson->sjson vjson)
+  "Convert sjson format to vjson
+Really this means converting vhashes to @-prefixed alists
+The opposite of sjson->vjson!"
+  (define (convert-list exp)
+    (map convert-json exp))
+
+  (define (convert-vlist exp)
+    (cons '@
+     (vhash-fold
+      (lambda (key val prev)
+        (cons (cons key (convert-json val))
+              (if (eq? prev vlist-null)
+                  '()
+                  prev)))
+      vlist-null
+      ;; cdr because first item is the @
+      exp)))
+
+  (define (convert-json exp)
+    (match exp
+      ((? vlist? exp)
+       (convert-vlist exp))
+      ((? pair? exp)
+       (convert-list exp))
+      (anything-else anything-else)))
+
+  (convert-json vjson))
