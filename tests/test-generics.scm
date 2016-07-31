9352378d9c964425e2dd36bd25976c1589750e25
Christopher Allan Webber <cwebber@dustycloud.org>
2016-07-31 18:15:29 -0500

    Add test-generics.scm

---
+++ tests/test-generics.scm
@@ -0,0 +1,23 @@
+(define-module (tests test-asobj)
+  #:use-module (srfi srfi-64)
+  #:use-module (pubstrate vocab)
+  #:use-module (pubstrate generics)
+  #:use-module ((pubstrate shorthand)
+                #:renamer (symbol-prefix-proc 'as:)))
+
+(test-begin "test-generics")
+
+(define-as-generic generic-foo "foo the bar")
+(define-as-method (generic-foo (asobj ^Object) x)
+  (list 'object-time asobj x))
+(define-as-method (generic-foo (asobj ^Activity) x)
+  (list 'activity-time asobj x))
+
+(let ((test-object (as:object)))
+  (test-equal (generic-foo test-object 'yup)
+    (list 'object-time test-object 'yup)))
+(let ((test-object (as:like)))
+  (test-equal (generic-foo test-object 'yup)
+    (list 'activity-time test-object 'yup)))
+
+(test-end "test-generics")
