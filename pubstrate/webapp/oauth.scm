23cdd7f07cf3e2dc3b7ccaff92e2656950f4b706
Christopher Allan Webber <cwebber@dustycloud.org>
2016-08-02 12:22:36 -0500

    Add oauth.scm with gen-bearer-token

---
+++ pubstrate/webapp/oauth.scm
@@ -0,0 +1,41 @@
+;;; Pubstrate --- ActivityStreams based social networking for Guile
+;;; Copyright © 2016 Christopher Allan Webber <cwebber@dustycloud.org>
+;;;
+;;; This file is part of Pubstrate.
+;;;
+;;; Pubstrate is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Pubstrate is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Pubstrate.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; OAuth (currently OAuth 2.0) support
+
+(define-module (pubstrate webapp oauth)
+  #:export (gen-bearer-token))
+
+(define *random-state* (random-state-from-platform))
+
+(define %token-chars
+  #(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m
+    #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z
+    #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M
+    #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z
+    #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
+
+(define %token-chars-length (vector-length %token-chars))
+
+(define (random-token-char)
+  (vector-ref %token-chars (random %token-chars-length)))
+
+(define* (gen-bearer-token #:optional (length 50))
+  (list->string
+   (map (lambda _ (random-token-char))
+        (iota length))))
