df4dbdaf1e8987e385be00697d909e48980f025c
Christopher Allan Webber <cwebber@dustycloud.org>
2016-08-08 10:10:40 -0500

    Add new shorthand access methods to asentry and relevant tests

--- pubstrate/webapp/asentry.scm
+++ pubstrate/webapp/asentry.scm
@@ -17,6 +17,7 @@
 ;;; along with Pubstrate.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (pubstrate webapp asentry)
+  #:use-module (ice-9 match)
   #:use-module (srfi srfi-9 gnu)
   #:use-module (pubstrate asobj)
   #:use-module (pubstrate vocab)
@@ -27,7 +28,9 @@
             asentry-private set-asentry-private
             asentry-id
 
-            asentry->sjson asentry->string string->asentry))
+            asentry->sjson asentry->string string->asentry
+            asentry-assoc asentry-ref
+            asentry-private-assoc asentry-private-ref))
 
 (define-immutable-record-type <asentry>
   (make-asentry-intern asobj private)
@@ -56,3 +59,20 @@
          (private (json-alist-ref str-sjson "private")))
     (make-asentry (make-asobj sjson asenv)
                   private)))
+
+(define (asentry-assoc key asentry)
+  "Shorthand for asobj-assoc on ASENTRY's asobj"
+  (asobj-assoc key (asentry-asobj asentry)))
+
+(define* (asentry-ref asentry key #:optional dflt)
+  "Shorthand for asobj-ref on ASENTRY's asobj"
+  (asobj-ref (asentry-asobj asentry) key dflt))
+
+(define (asentry-private-assoc key asentry)
+  (json-alist-assoc key (asentry-private asentry)))
+
+(define* (asentry-private-ref asentry key #:optional dflt)
+  (match (asentry-private-assoc key asentry)
+    ((_ . val)
+     val)
+    (#f dflt)))
