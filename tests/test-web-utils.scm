4918a25910727148312fb0df0ef37b6f0b1439da
Christopher Allan Webber <cwebber@dustycloud.org>
2016-08-07 20:19:03 -0500

    Add test-web-utils.scm

---
+++ tests/test-web-utils.scm
@@ -0,0 +1,21 @@
+(define-module (tests test-asobj)
+  #:use-module (srfi srfi-64)
+  #:use-module (web uri)
+  #:use-module (pubstrate webapp params)
+  #:use-module (pubstrate webapp utils))
+
+(test-begin "test-asobj")
+
+(parameterize ((%base-uri (string->uri "https://coolsite.example/")))
+  (test-equal (local-uri) "/")
+  (test-equal (local-uri "foo" "bar") "/foo/bar")
+  (test-equal (abs-local-uri "foo" "bar") "https://coolsite.example/foo/bar"))
+
+(parameterize ((%base-uri (string->uri "https://coolsite.example/stuff")))
+  (test-equal (local-uri) "/stuff")
+  (test-equal (local-uri "foo" "bar") "/stuff/foo/bar")
+  (test-equal (abs-local-uri "foo" "bar") "https://coolsite.example/stuff/foo/bar"))
+
+(test-end "test-asobj")
+
+(test-exit)
