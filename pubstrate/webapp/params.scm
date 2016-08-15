4f962598e8f2a4bbaab29196d2e683e41e004163
Christopher Allan Webber <cwebber@dustycloud.org>
2016-08-15 09:31:14 -0500

    Add some parameter setting utilities, of questionable usability :)

--- pubstrate/webapp/params.scm
+++ pubstrate/webapp/params.scm
@@ -17,8 +17,55 @@
 ;;; along with Pubstrate.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (pubstrate webapp params)
-  #:export (%user %store %base-uri))
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-39)
+  #:export (%user %store %base-uri
+
+            set-params! with-pubstrate-params))
 
 (define %user (make-parameter #f))
 (define %store (make-parameter #f))
 (define %base-uri (make-parameter #f))
+
+(define param-map
+  `((store . ,%store)
+    (user . ,%user)
+    (base-uri . ,%base-uri)))
+
+(define (get-param sym)
+  (assoc-ref param-map sym))
+
+(define (kwlist->alist kwlist)
+  (match kwlist
+    (((? keyword? kwarg) val rest ...)
+     (cons (cons (keyword->symbol kwarg) val)
+           (kwlist->alist rest)))
+    ('() '())))
+
+(define (set-params! . params)
+  "Set a list of keywords and values to their associated parameters."
+  (for-each
+   (match-lambda
+     ((key . val)
+      ((get-param key)
+       val)))
+   (kwlist->alist params)))
+
+;; @@: This function's api sucks and I don't even know if we need/want it
+(define (with-pubstrate-params params thunk)
+  (define unrolled-alist
+    (let lp ((alist (kwlist->alist params))
+             (key-list '())
+             (value-list '()))
+      (match alist
+        (((key . val) rest ...)
+         (lp rest
+             (cons key key-list)
+             (cons val value-list)))
+        ('()
+         (cons key-list value-list)))))
+  (define key-list (car unrolled-alist))
+  (define param-list
+    (map get-param key-list))
+  (define value-list (cdr unrolled-alist))
+  (with-parameters* param-list value-list thunk))
