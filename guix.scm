2bbb10e113c9d7debed5cfa16fa0acce44d2e5ca
Christopher Allan Webber <cwebber@dustycloud.org>
2016-08-17 14:45:59 -0500

    Add guix.scm for pubstrate

---
+++ guix.scm
@@ -0,0 +1,57 @@
+;;; Pubstrate --- ActivityStreams based social networking for Guile
+;;; Copyright © 2016 Christopher Allan Webber <cwebber@dustycloud.org>
+;;;
+;;; This file is part of Pubstrate.
+;;;
+;;; Pubstrate is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Pubstrate is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Pubstrate.  If not, see <http://www.gnu.org/licenses/>.
+
+(use-modules (guix packages)
+             (guix build-system gnu)
+             (guix gexp)
+             (gnu packages)
+             (gnu packages autotools)
+             (gnu packages base)
+             (gnu packages guile)
+             (gnu packages pkg-config)
+             (gnu packages texinfo)
+             (guix licenses))
+
+(define pubstrate
+  (package
+    (name "pubstrate")
+    (version "0.1-pre")
+    (source (local-file "." #:recursive? #t))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'bootstrap
+           (lambda _ (zero? (system* "sh" "bootstrap.sh")))))))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)
+       ("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("texinfo" ,texinfo)))
+    (inputs
+     `(("guile" ,guile-2.0)))
+    (propagated-inputs
+     `(("guile-gdbm-ffi" ,guile-gdbm-ffi)
+       ("guile-lib" ,guile-lib)))
+    (home-page #f)
+    (description "ActivityStreams and ActivityPub implementation in Guile.")
+    (synopsis "ActivityStreams and ActivityPub implementation in Guile.
+Includes a full (currently demo) web server.")
+    (license gpl3+)))
+
+pubstrate
