8728cef1fb70bc52b3fe5fc612746f99d678065e
Christopher Allan Webber <cwebber@dustycloud.org>
2017-05-17 15:35:22 -0500

    Add copyright header

--- pubstrate/aptestsuite.scm
+++ pubstrate/aptestsuite.scm
@@ -1,3 +1,21 @@
+;;; Pubstrate --- ActivityStreams based social networking for Guile
+;;; Copyright © 2017 Christopher Allan Webber <cwebber@dustycloud.org>
+;;;
+;;; This file is part of Pubstrate.
+;;;
+;;; Pubstrate is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Pubstrate is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Pubstrate.  If not, see <http://www.gnu.org/licenses/>.
+
 (use-modules (ice-9 control)
              (ice-9 match)
              (ice-9 format)
