6dc78842259503217ecfeac4dd21e83467516464
Christopher Allan Webber <cwebber@dustycloud.org>
2017-05-17 14:31:51 -0500

    updating copyright year

--- pubstrate/webapp/inbox-outbox.scm
+++ pubstrate/webapp/inbox-outbox.scm
@@ -1,5 +1,5 @@
  ;;; Pubstrate --- ActivityStreams based social networking for Guile
-;;; Copyright © 2016 Christopher Allan Webber <cwebber@dustycloud.org>
+;;; Copyright © 2016, 2017 Christopher Allan Webber <cwebber@dustycloud.org>
 ;;;
 ;;; This file is part of Pubstrate.
 ;;;
