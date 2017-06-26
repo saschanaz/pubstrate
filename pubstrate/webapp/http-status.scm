7fc791f2cc9330ba9d3dc5f3a6e82d3286ba3db2
Christopher Allan Webber <cwebber@dustycloud.org>
2017-06-26 15:52:17 -0500

    Add descriptions for all http status codes

--- pubstrate/webapp/http-status.scm
+++ pubstrate/webapp/http-status.scm
@@ -17,7 +17,9 @@
 ;;; along with Pubstrate.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (pubstrate webapp http-status)
-  #:export (continue switching-protocols processing
+  #:export (status-ref
+
+            continue switching-protocols processing
 
             ok created accepted non-authoritative-information
             no-content reset-content partial-content multi-status
@@ -46,81 +48,155 @@
 ;;; HTTP Status Codes.  For all your HTTP Status Code needs!
 ;;; https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 
+;; code => description
+(define status-codes
+  (make-hash-table))
+
+(define (status-ref code)
+  "Get the description for HTTP status code CODE.  Return #f if not known."
+  (hashv-ref status-codes code))
+
+(define-syntax-rule (define-status sym code description)
+  (begin
+    (define sym code)
+    (hashv-set! status-codes code description)))
+
 
 ;;; 1xx Informational
-(define continue 100)
-(define switching-protocols 101)
-(define processing 102)
+(define-status continue 100
+  "Continue")
+(define-status switching-protocols 101
+  "Switching Protocols")
+(define-status processing 102
+  "Processing")
 
 
 ;;; 2xx Success
-(define ok 200)
-(define created 201)
-(define accepted 202)
-(define non-authoritative-information 203)
-(define no-content 204)
-(define reset-content 205)
-(define partial-content 206)                  ; RFC 7233
-(define multi-status 207)                     ; WebDAV / RFC 4918
-(define already-reported 208)                 ; WebDAV / RFC 2842
-(define im-used 226)                          ; RFC 3229
+(define-status ok 200
+  "OK")
+(define-status created 201
+  "Created")
+(define-status accepted 202
+  "Accepted")
+(define-status non-authoritative-information 203
+  "Non-Authoritative Information")
+(define-status no-content 204
+  "No Content")
+(define-status reset-content 205
+  "Reset Content")
+(define-status partial-content 206                   ; RFC 7233
+  "Partial Content")
+(define-status multi-status 207                      ; WebDAV / RFC 4918
+  "Multi-Status")
+(define-status already-reported 208                  ; WebDAV / RFC 2842
+  "Already Reported")
+(define-status im-used 226                           ; RFC 3229
+  "IM Used")
 
 
 ;;; 3xx Redirection
-(define multiple-choices 300)
-(define moved-permanently 301)
-(define found 302)
-(define see-other 303)
-(define not-modified 304)                     ; RFC 7232
-(define use-proxy 305)                        ; dangerous!
-(define switch-proxy 306)                     ; deprecated
-(define temporary-redirect 307)
-(define permanent-redirect 308)               ; RFC 7538
+(define-status multiple-choices 300
+  "Multiple Choices")
+(define-status moved-permanently 301
+  "Moved Permanently")
+(define-status found 302
+  "Found")
+(define-status see-other 303
+  "See Other")
+(define-status not-modified 304                      ; RFC 7232
+  "Not Modified")
+(define-status use-proxy 305                         ; dangerous!
+  "Use Proxy")
+(define-status switch-proxy 306                      ; deprecated
+  "Switch Proxy")
+(define-status temporary-redirect 307
+  "Temporary Redirect")
+(define-status permanent-redirect 308                ; RFC 7538
+  "Permanent Redirect")
 
 
 ;;; 4xx Client Error
-(define bad-request 400)
-(define unauthorized 401)                     ; RFC 7235
-(define payment-required 402)
-(define forbidden 403)
-(define not-found 404)
-(define method-not-allowed 405)
-(define not-acceptable 406)
-(define proxy-auth-required 407)              ; RFC 7235
-(define request-timeout 408)
-(define conflict 409)
-(define gone 410)
-(define length-required 411)
-(define precondition-failed 412)              ; RFC 7232
-(define payload-too-large 412)                ; RFC 7231
-(define uri-too-long 414)                     ; RFC 7231
-(define unsupported-media-type 415)
-(define range-not-satisfiable 416)            ; RFC 7233
-(define expectation-failed 417)
-(define im-a-teapot 418)                      ; RFC 2324
-(define misdirected-request 421)              ; RFC 7540
-(define unprocessable-entity 422)             ; WebDAV / RFC 4918
-(define locked 423)             
-(define unprocessable-entity 422)             ; WebDAV / RFC 4918
-(define locked 423)                           ; WebDAV / RFC 4918
-(define failed-dependency 424)                ; WebDAV / RFC 4918
-(define upgrade-required 426)
-(define precondition-failed 428)              ; RFC 6585
-(define too-many-requests 429)                ; RFC 6585
-(define request-header-fields-too-large 431)  ; RFC 6585
-(define unavailable-for-legal-reasons 451)    ; RFC 7725
+(define-status bad-request 400
+  "Bad Request")
+(define-status unauthorized 401                      ; RFC 7235
+  "Unauthorized")
+(define-status payment-required 402
+  "Payment Required")
+(define-status forbidden 403
+  "Forbidden")
+(define-status not-found 404
+  "Not Found")
+(define-status method-not-allowed 405
+  "Method Not Allowed")
+(define-status not-acceptable 406
+  "Not Acceptable")
+(define-status proxy-auth-required 407               ; RFC 7235
+  "Proxy Authentication Required")
+(define-status request-timeout 408
+  "Request Timeout")
+(define-status conflict 409
+  "Conflict")
+(define-status gone 410
+  "Gone")
+(define-status length-required 411
+  "Length Required")
+(define-status precondition-failed 412               ; RFC 7232
+  "Precondition Failed")
+(define-status payload-too-large 412                 ; RFC 7231
+  "Payload Too Large")
+(define-status uri-too-long 414                      ; RFC 7231
+  "URI Too Long")
+(define-status unsupported-media-type 415
+  "Unsupported Media Type")
+(define-status range-not-satisfiable 416             ; RFC 7233
+  "Range Not Satisfiable")
+(define-status expectation-failed 417
+  "Expectation Failed")
+(define-status im-a-teapot 418                       ; RFC 2324
+  "I'm A Teapot")
+(define-status misdirected-request 421               ; RFC 7540
+  "Misdirected Request")
+(define-status unprocessable-entity 422              ; WebDAV / RFC 4918
+  "Unprocessable Entity")
+(define-status locked 423
+  "Locked")
+(define-status unprocessable-entity 422              ; WebDAV / RFC 4918
+  "Unprocessable Entity")
+(define-status failed-dependency 424                 ; WebDAV / RFC 4918
+  "Failed Dependency")
+(define-status upgrade-required 426
+  "Upgrade Required")
+(define-status precondition-failed 428               ; RFC 6585
+  "Precondition Failed")
+(define-status too-many-requests 429                 ; RFC 6585
+  "Too Many Requests")
+(define-status request-header-fields-too-large 431   ; RFC 6585
+  "Request Header Fields Too Large")
+(define-status unavailable-for-legal-reasons 451     ; RFC 7725
+  "Unavailable For Legal Reasons")
 
 
 ;;; 5xx Server Error
-(define internal-server-error 500)
-(define not-implemented 501)
-(define bad-gateway 502)
-(define service-unavailable 503)
-(define gateway-timeout 504)
-(define http-version-not-supported 505)
-(define variant-also-negotiates 506)          ; RFC 2295
-(define insufficient-storage 507)             ; WebDAV / RFC 5842
-(define loop-detected 508)                    ; WebDAV / RFC 5842
-(define not-extended 510)                     ; RFC 2274
-(define network-authentication-required 511)  ; RFC 6585
+(define-status internal-server-error 500
+  "Internal Server Error")
+(define-status not-implemented 501
+  "Not Implemented")
+(define-status bad-gateway 502
+  "Bad Gateway")
+(define-status service-unavailable 503
+  "Service Unavailable")
+(define-status gateway-timeout 504
+  "Gateway Timeout")
+(define-status http-version-not-supported 505
+  "HTTP Version Not Supported")
+(define-status variant-also-negotiates 506           ; RFC 2295
+  "Variant Also Negotiates")
+(define-status insufficient-storage 507              ; WebDAV / RFC 5842
+  "Insufficient Storage")
+(define-status loop-detected 508                     ; WebDAV / RFC 5842
+  "Loop Detected")
+(define-status not-extended 510                      ; RFC 2274
+  "Not Extended")
+(define-status network-authentication-required 511   ; RFC 6585
+  "Network Authentication Required")
 
